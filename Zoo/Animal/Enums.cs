e1349d3d1cc5b62051d732e1929b8ad44cd29b4f
Tee <saltandtee@gmail.com>
2022-03-29 08:41:08 +0200

    caretaker

--- Zoo/Animal/Enums.cs
+++ Zoo/Animal/Enums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Zoo.Animal
+﻿namespace Zoo.Animal
 {
     public enum Gender
     {
