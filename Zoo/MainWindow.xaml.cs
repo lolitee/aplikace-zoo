5b7616bd7a318af117850fc5fa16dd8390f79ea2
marty <riedlmarty@gmail.com>
2022-03-30 19:04:22 +0200

    DB

--- Zoo/MainWindow.xaml.cs
+++ Zoo/MainWindow.xaml.cs
@@ -27,8 +27,7 @@ namespace Zoo
         public MainWindow()
         {
             InitializeComponent();
-            db = new DB(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Martin\source\repos\lolitee\aplikace-zoo\databaze.mdf;Integrated Security=True;Connect Timeout=30");
-
+            db = new DB(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\škola\PGV4\Spolecna\Data\db.mdf;Integrated Security=True;Connect Timeout=30");
             Console.WriteLine("----");
             Console.WriteLine(
                 db.Query("Table").Select().Get()
