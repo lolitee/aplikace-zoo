e1e224eb111ad483ad5ae97a07f64cc5f833411f
Tee <saltandtee@gmail.com>
2022-04-01 15:00:20 +0200

    interfaces

--- Src/Models/Animal/Queries/GetAnimalList.cs
+++ Src/Models/Animal/Queries/GetAnimalList.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zoo.Database;
 
 namespace Zoo.Models.Animal.Queries
 {
-    internal class GetAnimalList
+    internal class GetAnimalList : IModel
     {
+        public DataSet GetData(DB db)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetSortedData(DB db)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Src/Models/Caretaker/Queries/GetCaretakerList.cs
+++ Src/Models/Caretaker/Queries/GetCaretakerList.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zoo.Database;
 
 namespace Zoo.Models.Caretaker.Queries
 {
-    internal class GetCaretakerList
+    internal class GetCaretakerList : IModel
     {
+        public DataSet GetData(DB db)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetSortedData(DB db)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
