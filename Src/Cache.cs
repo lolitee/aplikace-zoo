b8d73f7fc47f05b4f3298f7008f1efcf1198c6de
Tee <saltandtee@gmail.com>
2022-04-07 14:24:30 +0200

    cache

--- Src/Cache.cs
+++ Src/Cache.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Zoo
 {
-    public class Cache
+    public class Cache<TKey, TValue>
     {
+        private readonly Dictionary<TKey, CacheItem<TValue>> _cache = new Dictionary<TKey, CacheItem<TValue>>();
+
+        public void Store(TKey key, TValue value)
+        {
+            _cache[key] = new CacheItem<TValue>(value);
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (!_cache.ContainsKey(key)) return default;
+            var cached = _cache[key];
+            return cached.Value;
+        }
+    }
+
+    public class CacheItem<T>
+    {
+        public CacheItem(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; }
     }
 }
